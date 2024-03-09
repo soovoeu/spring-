6c016fcd1f777d149a34361685be8ba94e45dea1
subin <ysoovoeu@gmail.com>
2024-03-10 02:44:56 +0900

    D2

--- src/main/java/com/example/gitstudy2/D.java
+++ src/main/java/com/example/gitstudy2/D.java
@@ -1,4 +1,9 @@
 package com.example.gitstudy2;
 
 public class D {
+    public void test() {
+        System.out.println("D");
+        System.out.println("D");
+        System.out.println("D");
+    }
 }
